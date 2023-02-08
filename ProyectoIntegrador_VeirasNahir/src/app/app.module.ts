4aac125bdfdbc88154b564f92dea287fa942c2e4
Nahir Veiras <nahir_vei@hotmail.com>
2023-02-08 12:20:39 -0300

    Creacion de dos componentes(logoap, socila), modificacion css

--- ProyectoIntegrador_VeirasNahir/src/app/app.module.ts
+++ ProyectoIntegrador_VeirasNahir/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
